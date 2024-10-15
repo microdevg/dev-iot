ddcb736daca8c004405757eef620331de2d27a21
German Velardez <germanvelardez16@gmail.com>
2024-10-15 12:27:16 -0300

    Base del proyecto QMAX-IOT

---
+++ main/qmax-iot.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+
+
+
+
+void Task_main(void* param);
+
+
+
+
+void app_main(void)
+{
+    static int counter = 0;
+
+    xTaskCreate(&Task_main, "Demo Task", 3000, &counter, 5, NULL);
+
+}
+
+
+
+void Task_main(void* param)
+{
+    int* _counter = (int*)param;
+    while (true) {
+        printf("counter:%d\n",(*_counter));
+        (*_counter)++;
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
