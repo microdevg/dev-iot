8f300f568bb03eeae4b4e0a5639b1ef33d3ae413
German Velardez <germanvelardez16@gmail.com>
2024-10-16 22:51:44 -0300

    [Compila pero no lo he probado] Necesito el conector RS485. Leer documentacion.

--- main/qmax-iot.c
+++ main/qmax-iot.c
@@ -8,7 +8,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "display.h"
-
+#include "qmax-modbus.h"
 
 
 /**
@@ -21,25 +21,13 @@ void app_main(void)
     printf(" Oled test example\n");   
     int counter = 0;
     char buffer[50]={0};
+    modbus_init();
     display_init(  NULL, "Modbus RTU init", NULL);   
  while(1){
     sprintf(buffer,"counter: %d\n",counter++);
     printf(buffer);
 
-if(counter < 20)display_send_conn((const char*)buffer);
-else     display_send_conn("WiFi Desconectado");
 
     vTaskDelay(1000 / portTICK_PERIOD_MS);
-    if(counter == 10){
-        display_send_alarm("Alarma B\n");
-    }
-    if(counter == 14){
-        display_send_modbus("Modbus falled\n");
-    }
-    if(counter == 25){
-        display_send_alarm("Alarma Off");
-        display_send_modbus("Modbus Conectado");
-    }
-
     }
 }
---
+++ main/qmax-modbus.h
@@ -0,0 +1,8 @@
+#ifndef QMAX_MODBUS_H
+#define QMAX_MODBUS_H
+
+
+
+void modbus_init();
+
+#endif
